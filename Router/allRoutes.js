3dbd94bd103749823dec36afd7aa89b3b5a3619b
fxdstudiokg <fxd@studiokg.fr>
2024-04-03 12:07:16 +0200

    gestion de la page de reservation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,7 +8,8 @@ export const allRoutes = [
   new Route("/signup", "Connexion", "/pages/auth/signup.html"),
   new Route("/account", "Mon compte", "/pages/auth/account.html"),
   new Route("/editPassword", "Modifier mon mot de passe", "/pages/auth/editPassword.html"),
-  new Route("/allReservation", "Vos Réservations", "/pages/reservations/allReservation.html"),
+  new Route("/allReservation", "Vos réservations", "/pages/reservations/allReservation.html"),
+  new Route("/reservation", "Réserver", "/pages/reservations/reservation.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
