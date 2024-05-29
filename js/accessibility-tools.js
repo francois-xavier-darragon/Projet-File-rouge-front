7db9ac6d369cfa7d77871c5ecfdd804b46890e3c
fxdstudiokg <fxd@studiokg.fr>
2024-05-29 16:30:20 +0200

    Ajout de la possiblité de souligné les liens modifiction de la police et reinitialisation

--- js/accessibility-tools.js
+++ js/accessibility-tools.js
@@ -7,26 +7,14 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
     }
 });
 
-let fontSize = 22; 
+let fontSize = 18; 
 let maxFontSize = 32;
 
-document.querySelector('.accessibility-sidebar-item-resize-plus').addEventListener('click', function() {
-    
-    if (fontSize < maxFontSize) {
-        fontSize += 2; 
-        if (fontSize > maxFontSize) {
-            fontSize = maxFontSize;
-        }
-        document.querySelectorAll('p, h1, h2, a').forEach(function(element) {
-            element.style.fontSize = `${fontSize}px`;
-        });
-    }
-})
-
-document.querySelector('.accessibility-sidebar-item-resize-minus').addEventListener('click', function() {
-    if (fontSize > 20) { 
-        fontSize -= 2; 
-        document.querySelectorAll('p, h1, h2, a').forEach(function(element) {
+function adjustFontSize() {
+    document.querySelectorAll('p, h1, h2, a').forEach(function(element) {
+        if (document.body.classList.contains('readable-font')) {
+            element.style.fontSize = `${fontSize * 1.2}px`;
+        } else {
             if (element.tagName === 'H1') {
                 element.style.fontSize = '2.5rem';
             } else if (element.tagName === 'H2') {
@@ -34,7 +22,24 @@ document.querySelector('.accessibility-sidebar-item-resize-minus').addEventListe
             } else {
                 element.style.fontSize = `${fontSize}px`;
             }
-        });
+        }
+    });
+}
+
+document.querySelector('.accessibility-sidebar-item-resize-plus').addEventListener('click', function() {
+    if (fontSize < maxFontSize) {
+        fontSize += 2; 
+        if (fontSize > maxFontSize) {
+            fontSize = maxFontSize;
+        }
+        adjustFontSize();
+    }
+});
+
+document.querySelector('.accessibility-sidebar-item-resize-minus').addEventListener('click', function() {
+    if (fontSize > 20) { 
+        fontSize -= 2; 
+        adjustFontSize();
     }
 });
 
@@ -58,5 +63,26 @@ document.querySelector('.accessibility-sidebar-item-resize-light-background').ad
     document.documentElement.classList.toggle('light-background');
 });
 
+document.querySelector('.accessibility-sidebar-item-resize-underline-links').addEventListener('click', function() {
+    document.body.classList.toggle('underline-links');
+});
 
+document.querySelector('.accessibility-sidebar-item-resize-readable-font').addEventListener('click', function() {
+    document.body.classList.toggle('readable-font');
+    adjustFontSize();
+});
+
+function resetStyles() {
+    document.querySelectorAll('p, h1, h2, a').forEach(function(element) {
+        element.style.fontSize = '';
+    });
+
+    document.body.classList.remove('high-contrast', 'negative-contrast', 'light-background', 'underline-links', 'readable-font');
+
+    document.body.style.filter = '';
+}
+
+document.querySelector('.accessibility-sidebar-item-resize-reset').addEventListener('click', function() {
+    resetStyles();
+});
 
